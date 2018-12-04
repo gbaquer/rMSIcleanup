e3da821cafefb9a1beac52c80a4f09f9254be761
Gerard Baquer Gómez <baquer.gomez@gmail.com>
2018-12-04 17:45:58 +0100

    Include some example unit tests

--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -1,9 +1,42 @@
 context("test-general")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+#RUN CODE
+output_string <- rMSIcleanup::hello()
+elapsed_t=system.time(for(i in 1:10000) x <- mean(rt(1000, df = 4)))
+t=as.numeric(elapsed_t)[1]
+
+#NUMERIC
+a=2*2
+test_that("equal", {
+  expect_equal(a, 4)
+})
+test_that("less", {
+  expect_lt(a, 5)
+})
+test_that("less or equal", {
+  expect_lte(a, 4)
+})
+test_that("greater", {
+  expect_gt(a, 3)
+})
+test_that("greater or equal", {
+  expect_gte(a, 4)
 })
 
 test_that("multiplication works 2", {
-  expect_equal(2 * 2, 2)
+  expect_equal(a, a)
+})
+
+#CHECK OUTPUT STRING
+test_that("Not empty", {
+  expect_match(output_string, ".")
+})
+
+test_that("Includes world", {
+  expect_match(output_string, "world")
+})
+
+#CHECK RUNTIME
+test_that("Fast code", {
+  expect_lt(t, 3)
 })
